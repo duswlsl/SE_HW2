4308b29b19a9e338633082b6e8904f9ee2aca7b0
duswlsl <hongy3@naver.com>
2017-11-29 17:40:29 +0900

    modify main

--- main.java
+++ main.java
@@ -3,7 +3,12 @@ public class main{
 		Scanner scanner = new Scanner(System.in);
 
 		String[][] account = new String[50][3];
+		String[] memo = new String[50];
 		int accIndex = 0;
+		int memoIndex = 0;
+
+		Memo m = new Memo();
+		memoIndex = m.MemoFUnc(memo, memoIndex);
 
 		Account acc = new Account();
 		accIndex = acc.AccountFunc(account, accIndex);
